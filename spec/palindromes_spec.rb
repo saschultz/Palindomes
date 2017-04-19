a04fa7dbb62c3f266736292155a3f934f895bb25
Sara Schultz and Tyler Stephenson <sara@>
2017-04-19 14:11:36 -0700

    pass test for symbols

--- spec/palindromes_spec.rb
+++ spec/palindromes_spec.rb
@@ -25,4 +25,8 @@ describe('String#palindromes') do
   it('will compare a sentece') do
     expect(('Too hot to hoot').palindromes()).to(eq(true))
   end
+
+  it('will compare a sequence of characters or symbols') do
+    expect(('!@##@!').palindromes()).to(eq(true))
+  end
 end
