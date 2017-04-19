beb296d4e4ee57de41372f19e356109fd8be5210
Sara Schultz and Tyler Stephenson <tyler@>
2017-04-19 13:54:27 -0700

    add test and logic for testing words with three letters

--- lib/palindromes.rb
+++ lib/palindromes.rb
@@ -1,9 +1,21 @@
 class String
   define_method(:palindromes) do
-    if self[0] == self[1]
-      true
-    else
-      false
+    string_to_test = self.to_s
+    length = string_to_test.length
+    counted_length = length.to_f./(2).ceil()
+    booleans_array = []
+    index = 0
+
+    while index <= counted_length do
+      booleans_array.push(string_to_test[index].==(string_to_test[length.-(index.+(1))]))
+      index += 1
     end
+
+    if booleans_array.include?(false)
+      false
+    else
+      true
+    end
+
   end
 end
--- spec/palindromes_spec.rb
+++ spec/palindromes_spec.rb
@@ -2,7 +2,15 @@ require('rspec')
 require('palindromes')
 
 describe('String#palindromes') do
+  it('will return false if a word is not a palindrome') do
+    expect(("at").palindromes()).to(eq(false))
+  end
+
   it('will compare a two letter word') do
     expect(("aa").palindromes()).to(eq(true))
+  end
+
+  it('will compare a three letter word') do
+    expect(("pop").palindromes()).to(eq(true))
   end
 end
